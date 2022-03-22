834c42d65f58b2c9355f1993d27e9f4be325592e
ldmavrogonatou <101641766+ldmavrogonatou@users.noreply.github.com>
2022-03-22 12:56:00 +0000

    Add files via upload

---
+++ BDopt_optimisation.py
@@ -0,0 +1,198 @@
+import random
+import math
+import numpy.matlib
+import numpy as np
+from scipy.stats import ncx2
+from scipy.stats.distributions import chi2
+from set import *
+
+# objective function for D_A-optimality
+def objfunc(nalloc,a,sigma,priorSigma,ncur,K,T):
+
+     X = makeX(ncur,K,T,nalloc)
+     Z = makeZ(ncur,K,T,nalloc)
+     U = makeU(T,a)
+     Sigma = makeS(ncur,T,K,nalloc,sigma)
+
+     V = np.add(Sigma,np.dot(np.dot(Z,U),Z.T))
+
+     d = np.identity(ncur)
+     Vinv = np.linalg.solve(V, d)
+
+     M = np.dot(np.dot(X.T,Vinv),X)
+
+     dM = np.identity(K)
+     Minv = np.linalg.solve(M, dM)
+
+     A = np.zeros((K,1))
+     A[0][0] = -1
+     A[1][0] = 1
+     AMAT = np.dot(np.dot(A.T,Minv),A)
+
+     detAMAT = np.linalg.det(AMAT)
+     f = math.log(detAMAT)
+
+     return f
+
+# standard grid search optimisation to minimise the objective function for D_A-optimality (objfunc)
+def opt(N,Ntreat,Nblock,a,sigma,priorSigma):
+
+    if( Nblock==1 ):
+        fmin = 100
+        nmin = np.zeros((Nblock,Ntreat))
+        # perform search over the design space D={[1,2,...]x[1,2,...];i0+i1=N}
+        for i0 in range(1,N):
+            for i1 in range(1,N):
+                        if( i0+i1==N ):
+                            n = [[i0,i1]]
+                            f = objfunc(n,a,sigma,priorSigma,N,Ntreat,Nblock)
+                            if( f<fmin ):
+                                fmin = f
+                                nmin = np.copy(n)
+
+    if( Nblock==2 ):
+        fmin = 100
+        nmin = np.zeros((Nblock,Ntreat))
+        # perform search over the design space
+        for i0 in range(1,N):
+            for i1 in range(1,N):
+                for i2 in range(1,N):
+                    for i3 in range(1,N):
+                        if( i0+i1+i2+i3==N ):
+                            n = [[i0,i1],[i2,i3]]
+                            f = objfunc(n,a,sigma,priorSigma,N,Ntreat,Nblock)
+                            if( f<fmin ):
+                                fmin = f
+                                nmin = np.copy(n)
+
+    if( Nblock==3 ):
+        fmin = 100
+        nmin = np.zeros((Nblock,Ntreat))
+        # perform search over the design space
+        for i0 in range(1,N):
+            for i1 in range(1,N):
+                for i2 in range(1,N):
+                    for i3 in range(1,N):
+                        for i4 in range(1,N):
+                            for i5 in range(1,N):
+                                if( i0+i1+i2+i3+i4+i5==N ):
+                                    n = [[i0,i1],[i2,i3],[i4,i5]]
+                                    f = objfunc(n,a,sigma,priorSigma,N,Ntreat,Nblock)
+                                    if( f<fmin ):
+                                        fmin = f
+                                        nmin = np.copy(n)
+
+    return nmin
+
+# calculate power for certain p_0 and p_1 in [0,30]
+def power(nalloc,a,sigma,N,K,T,p,incr):
+
+    end = int(30.0/incr)
+    pwr = np.empty(end)
+    pos = 0
+
+    X = makeX(N,K,T,nalloc)
+    Z = makeZ(N,K,T,nalloc)
+    U = makeU(T,a)
+    Sigma = makeS(N,T,K,nalloc,sigma)
+    V = np.add(Sigma,np.dot(np.dot(Z,U),Z.T))
+
+    d = np.identity(N)
+    Vinv = np.linalg.solve(V, d)
+
+    M = np.dot(np.dot(X.T,Vinv),X)
+
+    dM = np.identity(K)
+    Minv = np.linalg.solve(M, dM)
+
+    A = np.zeros((K,1))
+    A[0][0] = -1
+    A[1][0] = 1
+    AMAT = np.dot(np.dot(A.T,Minv),A)
+
+    f = np.linalg.det(AMAT)
+
+    for i in range(end):
+        pc = p-pos
+        ncp = pow(pc,2)/f # *N
+        x = chi2.ppf(0.95,1)
+        pwr[i] = 1-ncx2.cdf(x, 1, ncp)
+        pos = pos+incr
+
+    return pwr
+
+# calculate power for certain p_0 and p_1 
+def powerind(nalloc,a,sigma,N,K,T,p0,p1):
+
+    X = makeX(N,K,T,nalloc)
+    Z = makeZ(N,K,T,nalloc)
+    U = makeU(T,a)
+    Sigma = makeS(N,T,K,nalloc,sigma)
+
+    V = np.add(Sigma,np.dot(np.dot(Z,U),Z.T))
+
+    d = np.identity(N)
+    Vinv = np.linalg.solve(V, d)
+
+    M = np.dot(np.dot(X.T,Vinv),X)
+
+    dM = np.identity(K)
+    Minv = np.linalg.solve(M, dM)
+
+    A = np.zeros((K,1))
+    A[0][0] = -1
+    A[1][0] = 1
+    AMAT = np.dot(np.dot(A.T,Minv),A)
+
+    f = np.linalg.det(AMAT)
+
+    pc = p1-p0
+    ncp = pow(pc,2)/f*N
+    x = chi2.ppf(0.95,1)
+    pwr = 1-ncx2.cdf(x, 1, ncp)
+
+    return pwr
+
+# BLUE estimator of treatment effects for Ntreats=2
+def est(n,sumy,a,sigma,N,K,T):
+
+    X = makeX(N,K,T,n)
+    Z = makeZ(N,K,T,n)
+    U = makeU(T,a)
+    Sigma = makeS(N,T,K,n,sigma)
+    V = np.add(Sigma,np.dot(np.dot(Z,U),Z.T))
+    d = np.identity(N)
+    Vinv = np.linalg.solve(V, d)
+    M = np.dot(np.dot(X.T,Vinv),X)
+    detM = np.linalg.det(M)
+
+    dt11 = M[0,0]
+    dt12 = M[0,1]
+    dt21 = M[1,0]
+    dt22 = M[1,1]
+
+    rho = np.zeros((T,K))
+    for i in range(T):
+        for j in range(K):
+            rho[i,j] = float(n[i][j])
+
+    g = np.zeros(T)
+    for i in range(T):
+        g[i] = 1.0 + a[i]*rho[i,0]/sigma[0] + a[i]*rho[i,1]/sigma[1]
+
+    dt11 = 0
+    dt22 = 0
+    dt12 = 0
+    for i in range(T):
+        dt11 += rho[i,0]/sigma[0]-(a[i]*pow(rho[i,0],2.0))/(pow(sigma[0],2.0)*g[i])
+        dt22 += rho[i,1]/sigma[1]-(a[i]*pow(rho[i,1],2.0))/(pow(sigma[1],2.0)*g[i])
+        dt12 -= rho[i,0]*rho[i,1]*a[i]/(g[i]*sigma[0]*sigma[1])
+    dt21 = dt12;
+    detM = dt11*dt22-dt12*dt21
+
+    psest = np.zeros(K)
+    for i in range(T):
+        psest[0] += ((dt22/sigma[0] - dt22*rho[i,0]*a[i]/(g[i]*pow(sigma[0],2.0)) + dt12*rho[i,1]*a[i]/(g[i]*sigma[0]*sigma[1]))*sumy[i][0] + (-dt22*rho[i,0]*a[i]/(g[i]*sigma[0]*sigma[1]) - dt12/sigma[1] + dt12*rho[i,1]*a[i]/(g[i]*pow(sigma[1],2.0)))*sumy[i][1])/(detM)
+        psest[1] += ((-dt21/sigma[0] + dt21*rho[i,0]*a[i]/(g[i]*pow(sigma[0],2.0)) - dt11*rho[i,1]*a[i]/(g[i]*sigma[0]*sigma[1]))*sumy[i][0] + (dt21*rho[i,0]*a[i]/(g[i]*sigma[0]*sigma[1]) + dt11/sigma[1] - dt11*rho[i,1]*a[i]/(g[i]*pow(sigma[1],2.0)))*sumy[i][1])/(detM)
+
+    return (psest[1]-psest[0])
---
+++ BDopt_simulation.py
@@ -0,0 +1,224 @@
+import random
+import time
+import math
+import numpy as np
+import numpy.matlib
+from BDopt import *
+
+Nsim = 500
+
+Nall = 24
+Ntreats = 2
+Nblocks = 1
+Nperb = Nall/Nblocks
+Nsubblocks = 6
+Npersubb = Nperb/Nsubblocks
+
+allp = [[5],[15]]
+
+allsigma = [[16,16,16,5.29,5.29,3.43],[1.61,3.43,5.29,1.61,3.43,1.61]]
+
+alla = [1]
+a = [alla[0]]
+aallsubb = np.ones(Nsubblocks)*alla[0]
+
+priormu = [0,0]
+priorSigma = np.diag(np.ones(Ntreats)*10)
+
+###################
+
+# allocate kth treatement arm
+def alloKthArm(k,arms_prob,sigma):
+    u = np.random.normal(0,math.sqrt(alla[0]))
+    return np.random.normal(arms_prob[k]+u,math.sqrt(sigma[k]))
+
+# generate simulated data
+def gensumy(nalloc,arms_prob,sigma,Ntreats,Nsubblocks):
+
+    sumy = np.zeros(np.shape(nalloc))
+    for i in range(Nsubblocks):
+        for j in range(Ntreats):
+            for k in range(int(nalloc[i][j])):
+                sumy[i,j] += alloKthArm(j,arms_prob,sigma)
+    return sumy
+
+# repeat for every scenario of response variances (sigma_0,sigma_1)
+for ss in range(allsigma.shape[1]):
+    sigmatrue = [math.pow(allsigma[0][ss],2.0),math.pow(allsigma[1][ss],2.0)]
+
+    # repeat for every scenario of treatment effects (p_0,p_1)
+    for ppp in range(allp.shape[1]):
+        arms_prob = [allp[0][ppp],allp[1][ppp]]
+        f = open("Dopt_N%i_b%i_p%i%i.txt_sigma%i%i" % (Nall,Nsubblocks,arms_prob[0],arms_prob[1],100*math.sqrt(sigmatrue[0]),100*math.sqrt(sigmatrue[1])), "w")
+
+        allvar = [[[0 for ___ in range(Ntreats)] for __ in range(Nsubblocks)] for _ in range(Nsim)]
+        S = [[[0 for ___ in range(Ntreats)] for __ in range(Nsubblocks)] for _ in range(Nsim)]
+        allS = [[0 for __ in range(Ntreats)] for _ in range(Nsim)]
+
+        allcount = [[[0 for ___ in range(Ntreats)] for __ in range(Nsubblocks)] for _ in range(Nsim)]
+        allsumcount=[0 for _ in range(Ntreats)]
+
+        sumy = [[[0 for ___ in range(Ntreats)] for __ in range(Nsubblocks)] for _ in range(Nsim)]
+
+        allest = np.zeros(Nsim)
+        allestN = np.zeros(Nsim)
+        allestb = np.zeros(Nsim)
+        allsdest = np.zeros(Nsim)
+
+        mse = 0
+        mseN = 0
+        mseb = 0
+
+        # repeat for Nsim simulated datasets
+        for n in range(Nsim):
+
+            # asign prior distributions
+            mustar = priormu
+            Sigmastar = priorSigma
+
+            noptall = np.zeros((Nsubblocks,Ntreats))
+            sigma = [0,0] # store the estimates of sigma
+
+            sumcount = [0 for _ in range(Ntreats)]
+            for k in range(Ntreats):
+                S[n][0][k] = 0
+
+            # initial cohort (0) is equally randomised to the Ntreats treatments
+            count = [0 for _ in range(Ntreats)]
+            tempS = [0.0 for _ in range(Ntreats)]
+            storeS = np.zeros((Nperb,2))
+            while ( count[0]<=1 or count[1]<=1 ):
+                count = [0.0 for _ in range(Ntreats)]
+                tempS = [0.0 for _ in range(Ntreats)]
+                for tt in range(Npersubb):
+                    arm_choice = np.random.randint(2, size=1) # allocate patient tt randomly to one of the treatments with equal probability
+                    count[arm_choice] += 1                    # count total number of patients per treatment within cohort 0
+                    res = alloKthArm(arm_choice,arms_prob,sigmatrue) # generate observation for patient tt allocated to treatment arm_choice
+                    tempS[arm_choice] += res                          # sum up all patient outcomes for treatment arm_choice in order to obtain the mean
+                    storeS[tt][0] = arm_choice
+                    storeS[tt][1] = res
+
+            # store information for summary statistics
+            for k in range(Ntreats):
+                allcount[n][0][k] = count[k]
+                noptall[0,k] = count[k]
+                sumcount[k] += count[k]
+                allsumcount[k] += count[k]
+                S[n][0][k] = tempS[k]/count[k]
+                sumy[n][0][k] = tempS[k]
+                sigma[k] = np.var(storeS[np.argwhere(storeS[:Npersubb,0]==k),1]) # calculate the outcome variance based on observations
+                allvar[n][0][k] = sigma[k]
+
+            indices_value = [0 for _ in range(Ntreats)]
+            # repeat process for the remaining cohorts within the current block
+            for t in range(1, Nsubblocks):
+
+                    ncurp = t*Npersubb
+                    nopt = np.zeros((1,Ntreats)) ## 1 is because we observe 1 subblock per time
+
+                    # optimisation to find the D_A-optimal allocation
+                    nopt = opt(Npersubb,Ntreats,1,a,sigma,priorSigma)
+                    noptall[t,] = nopt[0,]
+
+                    count = [0.0 for _ in range(Ntreats)]
+                    tempS = [0.0 for _ in range(Ntreats)]
+                    count = 0
+                    # simulate observations for each patient in each treatment
+                    for i in range(Ntreats):
+                        for j in range(nopt[0,i]):
+                            res = alloKthArm(i,arms_prob,sigmatrue)
+                            tempS[i] += res
+                            storeS[t*Npersubb+count][0] = i
+                            storeS[t*Npersubb+count][1] = res
+                            count = count+1
+
+                    for kkk in range(Ntreats):
+                        sumcount[kkk] += nopt[0,kkk]
+                        allsumcount[kkk] += nopt[0,kkk]
+                        allcount[n][t][kkk] = nopt[0,kkk]
+                        sigma[kkk] = np.var(storeS[np.argwhere(storeS[:(t+1)*Npersubb,0]==kkk),1]) # calculate the outcome variance based on observations
+                        S[n][t][kkk] = tempS[kkk]/nopt[0,kkk]
+                        sumy[n][t][kkk] = tempS[kkk]
+                        allvar[n][t][kkk] = sigma[kkk]
+
+            # Calculate power
+            incr = 1.0
+            end = int(30.0/incr)
+            # for response-adaptive D_A-optimal allocation
+            pwr = power(noptall,aallsubb,sigmatrue,Nall,Ntreats,Nsubblocks,15,incr)
+
+            # for non-adaptive D_A-optimal (Neyman) allocation
+            NNeyman = int((Nall/Nsubblocks)*math.sqrt(sigmatrue[0])/(math.sqrt(sigmatrue[0])+math.sqrt(sigmatrue[1])))
+            nNeyman = [[NNeyman,Nall/Nsubblocks-NNeyman],[NNeyman,Nall/Nsubblocks-NNeyman],[NNeyman,Nall/Nsubblocks-NNeyman],[NNeyman,Nall/Nsubblocks-NNeyman],[NNeyman,Nall/Nsubblocks-NNeyman],[NNeyman,Nall/Nsubblocks-NNeyman]]
+            pwrNeyman = power(nNeyman,aallsubb,sigmatrue,Nall,Ntreats,Nsubblocks,15,incr)
+
+            # for balanced allocation
+            Nbal = Nall/Ntreats/Nsubblocks
+            nbal = [[Nbal,Nbal],[Nbal,Nbal],[Nbal,Nbal],[Nbal,Nbal],[Nbal,Nbal],[Nbal,Nbal]]
+            pwrbal = power(nbal,aallsubb,sigmatrue,Nall,Ntreats,Nsubblocks,15,incr)
+
+            # Estimation of treatment effect and calculation of mean squared error
+            Nsimest = 10000
+            tempest = np.zeros(Nsimest)
+            for i in range(Nsimest):
+
+                sy = np.zeros(np.shape(noptall))
+                sy = gensumy(noptall,arms_prob,sigmatrue,Ntreats,Nsubblocks)
+                tempest[i] = est(noptall,sy,aallsubb,sigmatrue,Nall,Ntreats,Nsubblocks)
+                mse += math.pow(tempest[i]-(allp2[0]-allp[0]),2.0)/(Nsimest*Nsim)
+
+            allest[n] = sum(tempest)/Nsimest
+            allsdest[n] = np.std(tempest)
+
+            #if( t==(Nsubblocks-1) ):
+                #print(str(t)+' --- sigma = ('+str(sigma[0])+','+str(sigma[1])+')')
+            # for tt in range(Nsubblocks):
+            #     f.write("%.3f %.3f " % (S[n][tt][0],S[n][tt][1]))
+            # for tt in range(Nsubblocks):
+            #     f.write("%.3f %.3f " % (allvar[n][tt][0],allvar[n][tt][1]))
+            # for tt in range(Nsubblocks):
+            #     f.write("%.1f %.1f " % (allcount[n][tt][0],allcount[n][tt][1]))
+            # for tt in range(end):
+            #     f.write("%.3f " % (pwr[tt]))
+            # for tt in range(end):
+            #     f.write("%.3f " % (pwrNeyman[tt]))
+            # for tt in range(end):
+            #     f.write("%.3f " % (pwrbal[tt]))
+            # f.write("%.3f %.3f %.3f" % (fBDopt,fNeyman,fbal))
+            # #f.write("%.1f %.1f" % (sumcount[0],sumcount[1]))
+            # f.write("\n")
+
+        tempestN = np.zeros(Nsimest)
+        tempestb = np.zeros(Nsimest)
+        for i in range(Nsimest):
+
+            sy = gensumy(nNeyman,arms_prob,sigmatrue,Ntreats,Nsubblocks)
+            tempestN[i] = est(nNeyman,sy,aallsubb,sigmatrue,Nall,Ntreats,Nsubblocks)
+            mseN += math.pow(tempestN[i]-(allp2[0]-allp[0]),2.0)/Nsimest
+
+            sy = gensumy(nbal,arms_prob,sigmatrue,Ntreats,Nsubblocks)
+            tempestb[i] = est(nbal,sy,aallsubb,sigmatrue,Nall,Ntreats,Nsubblocks)
+            mseb += math.pow(tempestb[i]-(allp2[0]-allp[0]),2.0)/Nsimest
+
+        mest = sum(allest)/Nsim
+        stdest = sum(allsdest)/Nsim
+        mestN = sum(tempestN)/Nsimest
+        stdestN = np.std(tempestN)
+        mestb = sum(tempestb)/Nsimest
+        stdestb = np.std(tempestb)
+
+        # for individual power calculations
+        #pwr = powerind(noptall,aallsubb,sigmatrue,Nall,Ntreats,Nsubblocks,5,15)
+        #pwrN = powerind(nNeyman,aallsubb,sigmatrue,Nall,Ntreats,Nsubblocks,5,15)
+        #pwrb = powerind(nbal,aallsubb,sigmatrue,Nall,Ntreats,Nsubblocks,5,15)
+
+        #f.write("%.3f %.3f %.3f %.3f %.3f %.3f" % (mest,stdest,mestN,stdestN,mestb,stdestb))
+        f.write("%.3f %.3f %.3f %.3f %.3f \n" % (allsigma0[ss],allsigma1[ss],mse,mseN,mseb))
+
+        print('('+str(allsigma0[ss])+','+str(allsigma1[ss])+') & '+str(round(mest,2))+' ('+str(round(stdest,2))+')'+'& '+str(round(mestN,2))+' ('+str(round(stdestN,2))+')'+'& '+str(round(mestb,2))+' ('+str(round(stdestb,2))+')'+'& '+str(pwr)+'& '+str(pwrN)+'& '+str(pwrb))
+        #print("n = ("+str(allsumcount[0]/Nsim)+" , "+str(allsumcount[1]/Nsim)+")")
+        #f.write("%.3f,%.3f,%.1f %.1f" % (sigmatrue[0],sigmatrue[1],allsumcount[0]/Nsim,allsumcount[1]/Nsim))
+        #f.write("\n")
+
+        #f.close()
+f.close()
